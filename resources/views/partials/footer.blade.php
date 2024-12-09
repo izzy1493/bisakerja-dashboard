6f9124fb0ef5d6548e51f33c489942036b75dae9
Christianim <christianim.intern@gmail.com>
2024-12-09 15:32:48 +0700

    update tampilan pencari

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- resources/views/partials/footer.blade.php -->
+<footer class="bg-gray-800 text-white py-6">
+    <div class="max-w-7xl mx-auto px-6 text-center">
+        <p>&copy; 2024 JobConnect. All Rights Reserved.</p>
+    </div>
+</footer>
