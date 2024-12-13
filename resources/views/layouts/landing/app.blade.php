4cc217a6bb113c5095838372ed70046944c21cb2
ferdi7inc <ferdi7inc@gmail.com>
2024-12-14 00:02:00 +0700

    update develop

--- resources/views/layouts/landing/app.blade.php
+++ resources/views/layouts/landing/app.blade.php
@@ -25,16 +25,16 @@
 </head>
 
 <body class="bg-white font-sans leading-relaxed text-gray-900">
-@yield('content')
 
 <!-- Navbar -->
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="#" class="text-3xl font-semibold text-blue-600">JobConnect</a>
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-        <!-- Tombol Gabung Mitra -->
-        <button type="button" class="text-white bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">LOGIN</button>
         
+        <!-- Tombol LOGIN -->
+        <a href="/login" class="text-white bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm me-2 px-4 py-2 text-center dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">LOGIN</a>
+
         <!-- Tombol Sign Up -->
         <a href="/signup" class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">DAFTAR</a>
 
@@ -63,12 +63,16 @@
   </div>
 </nav>
 
+@yield('content')
+
+
 <!-- Footer -->
 <footer class="bg-gray-800 text-white py-6">
     <div class="max-w-7xl mx-auto px-6 text-center">
         <p>&copy; 2024 JobConnect. All Rights Reserved.</p>
     </div>
 </footer>
+<script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
 
 </body>
 
